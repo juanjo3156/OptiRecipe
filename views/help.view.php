57e6b772d7dd61a06e50c1edaf26bcf801415cfb
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-14 12:09:53 -0600

    creando help.php junto con su vista y su scss

---
+++ views/help.view.php
@@ -0,0 +1,11 @@
+<?php include('templates/header.php');?>
+<div class="instructions container">
+    <h2 class="instructions__tittle">
+        ¿Cómo usar OptiRecipe?
+    </h2>
+    <div class="instructions__content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, sed culpa beatae laborum, voluptatibus doloremque nisi excepturi debitis minus recusandae, officia ipsam aperiam aliquid provident natus quia id repellendus magnam?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, sed culpa beatae laborum, voluptatibus doloremque nisi excepturi debitis minus recusandae, officia ipsam aperiam aliquid provident natus quia id repellendus magnam?</p>
+    </div>
+</div>
+<?php include('templates/footer.php'); ?>
