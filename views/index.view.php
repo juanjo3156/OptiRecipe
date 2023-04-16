11d317eec8eb9619c9eadda69ef40712d8f43d07
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-16 16:12:44 -0600

    añadiendo el menu principal y sus estilos

--- views/index.view.php
+++ views/index.view.php
@@ -1,18 +1,26 @@
 <?php include('templates/header.php');?>
 
-<h2 class="container">Escoge una opción</h2>
+    <h2 class="container tittle">Escoge una opción</h2>
 <div class="container content">
     <div class="content__grid">
         <a href="#"class="content__option">
             <div>
-                <p>Nueva Receta</p>
-                <i class="fa-solid fa-sheet-plastic"></i>
+                <p>Consultar Recetas</p>
+                <i class="fa-solid fa-list"></i>
             </div>
         </a>
         <a href="#"class="content__option">
             <div>
-                <p>Consultar Recetas</p>
-                <i class="fa-solid fa-list"></i>
+                
+                <p>Nueva Receta</p>
+                <i class="fa-solid fa-sheet-plastic"></i>
+            </div>
+            
+        </a>
+        <a href="#"class="content__option">
+            <div>
+                <p>Facturas pendientes</p>
+                <i class="fa-solid fa-file-invoice"></i>
             </div>
             
         </a>
@@ -20,6 +28,7 @@
             <div>
                 <p>Configuraciones</p>
                 <i class="fa-solid fa-sliders"></i>
+                
             </div>
             
         </a>
