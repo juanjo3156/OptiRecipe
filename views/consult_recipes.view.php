c123aa686e5f5028bc5889461421c4724c154b32
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-22 08:36:35 -0600

    adding a table to consult rcipes view

--- views/consult_recipes.view.php
+++ views/consult_recipes.view.php
@@ -1,7 +1,27 @@
 <?php require('templates/header.php'); ?>
-    <div class="recipes container">
-        <div class="">
 
+        <div class="clients__table container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Clientes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Juan José Ruiz Cruz</td>
+                    </tr>
+                    <tr>
+                        <td>Juan José Ruiz Cruz</td>
+                    </tr>
+                    <tr>
+                        <td>Juan José Ruiz Cruz</td>
+                    </tr>
+                    <tr>
+                        <td>Juan José Ruiz Cruz</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    </div>
+
 <?php require('templates/footer.php'); ?>
