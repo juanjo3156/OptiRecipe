b80da8d592160f080f2b90067bb9d54f4e62c113
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-04-22 21:03:17 -0600

    consult recipes modifiying

--- views/consult_recipes.view.php
+++ views/consult_recipes.view.php
@@ -4,16 +4,18 @@
             <table class="table__content">
                 <thead class="table__head">
                     <tr>
-                        <th class="table__head-element"><h2>Clientes</h2></th>
+                        <th class="table__head-element"><h2>Paciente</h2></th>
                         <th class="table__head-element"><h2>Acciones</h2></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr class="table__row">
                         <td>Juan José Ruiz Cruz</td>
-                        <td class="table__actions"><a href="#" class="button" >Boton</a>
-                        <a href="#"class="button-red">Boton</a>
-                        <a href="#"class="button-green">Boton</a></td>
+                        <td class="table__actions">
+                            <a href="#" class="button" ><i class="fa-solid fa-eye"></i></a>
+                            <a href="#" class="button-green"><i class="fa-solid fa-user-pen"></i></a>
+                            <a href="#" class="button-red"><i class="fa-solid fa-trash"></i></a>
+                        </td>
                     </tr>
                 </tbody>
             </table>
