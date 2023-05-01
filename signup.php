2be555469f4d9613f59ca31ff996839513f0b912
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-01 17:50:58 -0600

    agregando comprobación de campos ya ingresados a la base de datos

--- signup.php
+++ signup.php
@@ -1,13 +1,36 @@
 <?php 
 require('config/config.php');
-require('scripts/db.php');
 require('scripts/functions.php');
-
+    $error = '';
     if($_SERVER['REQUEST_METHOD']=="POST"){
         $username = clean_data(strtolower($_POST['username']));
         $password = $_POST['password'];
         $password2 = $_POST['password2'];
-        $email = $_POST['password2'];
+        $email = $_POST['email'];
+        if(empty($username) or empty($password) or empty($password2) or empty($email)){
+            
+            $error.= '<li>Por favor llena todos los campos</li>';
+
+        }else{
+            require('scripts/db.php');
+            $statement = $connection->prepare("SELECT * FROM users WHERE username = :username LIMIT 1");
+            $statement->bindParam(":username",$username);
+            $statement->execute();
+            $result = $statement->fetch();
+    
+            if($result != false){
+                $error.='<li>El nombre de usuario ya existe</li>';
+            }
+
+            $statement = $connection->prepare("SELECT * FROM users WHERE email = :email LIMIT 1");
+            $statement->bindParam(":email",$email);
+            $statement->execute();
+            $result = $statement->fetch();
+
+            if($result != false){
+                $error.='<li>El email ya ha sido utilizado por otro usuario</li>';
+            }
+        }
     }
 
 
