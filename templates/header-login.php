ab476bf5acb038ef6ca07d7dc447fe2b663015ba
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-01 12:55:26 -0600

    haciendo un header para el login y el signup

---
+++ templates/header-login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
+    <title>OptiRecipe - Inicio</title>
+    <script src="https://kit.fontawesome.com/9324a5a112.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header class ="header container">
+        <a href="<?php echo RUTA?>index.php">
+            <h1 class="header__title">OptiRecipe</h1>
+        </a>
+        <a href="<?php echo RUTA?>help.php">
+            <h3 class="header__help"><i class="fa-solid fa-question"></i></h3>
+        </a>
+    </header>
