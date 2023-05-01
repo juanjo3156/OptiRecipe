0110785f7c33e0460da68bb5a14585e12f811998
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-01 13:28:17 -0600

    añadiendo boton de iniciar sesion

--- signup.php
+++ signup.php
@@ -1,2 +1,14 @@
-<?php require('config/config.php');
+<?php 
+require('config/config.php');
+require('scripts/db.php');
+require('scripts/functions.php');
+
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        $username = clean_data(strtolower($_POST['username']));
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        $email = $_POST['password2'];
+    }
+
+
 require('views/signup.view.php') ?>
