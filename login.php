03fc429dc9ebe7c68fd24688cdb0079c8f1f3b98
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-01 13:29:04 -0600

    agregando manejo de errores

--- login.php
+++ login.php
@@ -10,18 +10,23 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $username = clean_data(filter_var(strtolower($_POST['username']),FILTER_SANITIZE_STRING));
     $password = $_POST['password'];
     $password = $password;
-    $statement = $connection->prepare('SELECT * FROM users WHERE username = :username AND password = :password ');
-    $statement->execute(array(":username"=>$username,":password"=>$password));
-    $result = $statement->fetch();
-
-    if($result != false){
-        $_SESSION['user'] = $username;
-        echo "usuario conectado";
-
-        header('Location:'.RUTA.'index.php');
-    }else{
-        $error.= '<li>Incorrect username or password</li>';
-    }
+    
+        $statement = $connection->prepare('SELECT * FROM users WHERE username = :username AND password = :password ');
+        $statement->execute(array(":username"=>$username,":password"=>$password));
+        $result = $statement->fetch();
+        if(empty($username) or empty($password)){
+            $error.= '<li>Por favor llena todos los campos</li>';
+        }else{
+            if($result != false){
+                $_SESSION['user'] = $username;
+                echo "usuario conectado";
+        
+                header('Location:'.RUTA.'index.php');
+            }else{
+                $error.= '<li>Nombre de usuario o contraseña incorrectos</li>';
+            }
+        }
+   
     
 }
 require('views/login.view.php');
