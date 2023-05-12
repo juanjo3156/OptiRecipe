a3970f6e460bf7da3985df63ee2a4a43ee08477f
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-12 12:20:23 -0600

    añadiendo edición de las recetas

---
+++ edit_recipe.php
@@ -0,0 +1,78 @@
+<?php
+    session_start();
+    require("config/config.php");
+    require("scripts/set_session.php");
+    require("scripts/db.php");
+    $error = "";
+    if(isset($_GET['patient_id']) && isset($_GET["recipe_id"])){
+        $patient_id = $_GET["patient_id"];
+        $recipe_id = $_GET["recipe_id"];
+        $_SESSION["patient_id"] = $patient_id;
+        $_SESSION["recipe_id"] = $recipe_id;
+        
+    }else if(isset($_SESSION["patient_id"]) && isset($_SESSION["recipe_id"])){
+        $patient_id = $_SESSION["patient_id"];
+        $recipe_id = $_SESSION["recipe_id"];
+    }
+    if(!empty($patient_id) && !empty($recipe_id)){
+        $statement = $connection->prepare("SELECT * FROM prescriptions WHERE prescription_id = :recipe_id LIMIT 1");
+        $statement->bindParam(":recipe_id", $recipe_id);
+        $statement->execute();
+        $recipe_info = $statement->fetch();
+        
+        $statement = $connection->prepare("SELECT * FROM patients WHERE patient_id = :patient_id LIMIT 1");
+        $statement->bindParam(":patient_id", $patient_id);
+        $statement->execute();
+        $patient_info = $statement->fetch();
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(isset($_POST)){
+            $date = $_POST["date"];
+            $glass_type = $_POST["glass_type"];
+            $sphere_right = $_POST["sphere_right"];
+            $sphere_left = $_POST["sphere_left"];
+            $cylinder_right = $_POST["cylinder_right"];
+            $cylinder_left = $_POST["cylinder_left"];
+            $axis_right = $_POST["axis_right"];
+            $axis_left = $_POST["axis_left"];
+            $height_right = $_POST["height_right"];
+            $height_left = $_POST["height_left"];
+            $addition_right = $_POST["addition_right"];
+            $addition_left = $_POST["addition_left"];
+            $dnp_right = $_POST["dnp_right"];
+            $dnp_left = $_POST["dnp_left"];
+            $notes = $_POST["notes"];
+            $price = $_POST["price"];
+
+            if(empty($date)||empty($price)){
+                $error.= "<li>El nombre, la fecha de nacimiento y el numero de teléfono son obligatorios</li>";
+            }else{
+                $statement = $connection->prepare("UPDATE prescriptions SET date=:date, glass_type=:glass_type,sphere_right = :sphere_right,sphere_left = :sphere_left,cylinder_right =:cylinder_right,cylinder_left = :cylinder_left,axis_right = :axis_right, axis_left = :axis_left, height_right = :height_right,height_left = :height_left,addition_right=:addition_right,addition_left = :addition_left,dnp_right = :dnp_right,dnp_left = :dnp_left, notes = :notes, price = :price WHERE patient_id = :patient_id AND prescription_id = :prescription_id");
+                $statement->bindParam(":patient_id",$patient_id);
+                $statement->bindParam(":prescription_id",$recipe_id);
+                $statement->bindParam(':date', $date);
+                $statement->bindParam(':glass_type', $glass_type);
+                $statement->bindParam(':sphere_left', $sphere_left);
+                $statement->bindParam(':sphere_right', $sphere_right);
+                $statement->bindParam(':cylinder_left', $cylinder_left);
+                $statement->bindParam(':cylinder_right', $cylinder_right);
+                $statement->bindParam(':axis_left', $axis_left);
+                $statement->bindParam(':axis_right', $axis_right);
+                $statement->bindParam(':addition_left', $addition_left);
+                $statement->bindParam(':addition_right', $addition_right);
+                $statement->bindParam(':dnp_left', $dnp_left);
+                $statement->bindParam(':dnp_right', $dnp_right);
+                $statement->bindParam(':height_left', $height_left);
+                $statement->bindParam(':height_right', $height_right);
+                $statement->bindParam(':notes', $notes);
+                $statement->bindParam(':price', $price);
+
+                $statement->execute();
+    
+                header("Location:".RUTA."consult_recipe.php?txtID=".$patient_id);
+            }
+        }
+    }
+    
+    require("views/edit_recipe.view.php"); ?>
---
+++ views/edit_recipe.view.php
@@ -0,0 +1,82 @@
+<?php require("templates/header.php") ?>
+<div class="container form_new_container">
+    <h2>Nueva Receta</h2>
+    <i class="fa-solid fa-file-circle-plus"></i>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" class="form-recipe" method="post">
+        <div class="form-recipe__field">
+            <label for="pacient_name">Paciente:</label>
+            <input disabled id="pacient__name" type="text" value="<?php echo $patient_info["name"]?>">
+        </div>
+        <div class="form-recipe__field">
+            <label for="creation_date">Fecha:</label>
+            <input id="creation_date" type="date" name="date" value="<?php echo $recipe_info["date"]?>">
+        </div>
+        <div class="form-recipe__field">
+            <label for="glass_type">Tipo de lente:</label>
+            <select id="glass_type" name="glass_type" value="<?php echo $recipe_info["glass_type"]?>">
+                <!-- <option selected disabled>--seleccionar--</option> -->
+                <option value="Monofocal" <?php if ($recipe_info["glass_type"] == "Monofocal") echo "selected"; ?>>Monofocal</option>
+                <option value="Bifocal" <?php if ($recipe_info["glass_type"] == "Bifocal") echo "selected"; ?>>Bifocal</option>
+                <option value="Progresiva"<?php if ($recipe_info["glass_type"] == "Progresiva") echo "selected"; ?>>Progresiva</option>
+            </select>
+        </div>
+            <div class="form-recipe__field">
+            <table class="graduation_table">
+                <tr>
+                    <th></th>
+                    <th>Esf</th>
+                    <th>Cil</th>
+                    <th>Eje</th>
+                    <th>Alt</th>
+                    <th>Add</th>
+                    <th>Dnp</th>
+                </tr>
+                <tr>
+                <th>OD</th>
+                    <td><input type="text" id="esfera_od" name="sphere_right" value="<?php echo $recipe_info["sphere_right"]?>"></td>
+                    <td><input type="text" id="cilindro_od" name="cylinder_right" value="<?php echo $recipe_info["cylinder_right"]?>"></td>
+                    <td><input type="text" id="eje_od" name="axis_right" value="<?php echo $recipe_info["axis_right"]?>"></td>
+                    <td><input type="text" id="altura_od" name="height_right" value="<?php echo $recipe_info["height_right"]?>"></td>
+                    <td><input type="text" id="add_od" name="addition_right" value="<?php echo $recipe_info["addition_right"]?>"></td>
+                    <td><input type="text" id="dnp_od" name="dnp_right" value="<?php echo $recipe_info["dnp_right"]?>"></td>
+                </tr>
+                <tr>
+                <th>OI</th>
+                    <td><input type="text" id="esfera_oi" name="sphere_left" value="<?php echo $recipe_info["sphere_left"]?>"></td>
+                    <td><input type="text" id="cilindro_oi" name="cylinder_left" value="<?php echo $recipe_info["cylinder_left"]?>"></td>
+                    <td><input type="text" id="eje_oi" name="axis_left" value="<?php echo $recipe_info["axis_left"]?>"></td>
+                    <td><input type="text" id="altura_oi" name="height_left" value="<?php echo $recipe_info["height_left"]?>"></td>
+                    <td><input type="text" id="add_oi" name="addition_left" value="<?php echo $recipe_info["addition_left"]?>"></td>
+                    <td><input type="text" id="dnp_oi" name="dnp_left" value="<?php echo $recipe_info["dnp_left"]?>"></td>
+                </tr>
+            </table>
+            </div>
+            <div class="form-recipe__field">
+            <label for="notes">Diagnostico:</label>
+            <textarea name="notes" id="notes" cols="35" rows="4" ><?php echo $recipe_info["notes"]?></textarea>
+        </div>
+        <div class="form-recipe__field">
+            <label for="price">Precio:</label>
+            <div class="price_content">
+                <label for="">$</label>
+                <input class="price_input" id="price" name="price" type="text" placeholder="precio total" value="<?php echo $recipe_info["price"]?>" >
+            </div>
+        </div> 
+        <?php if($error!=''):?>
+                <div class="error">
+                    <ul>
+                        <?php echo $error?>
+                    </ul>
+                </div>
+            <?php endif;?> 
+        <div class="form-recipe__field">
+            <input type="hidden" name="txtID" value="<?php echo $txtID; ?>">
+            <input type="submit" class="save_button" value="Actualizar receta">
+            
+            <a href="<?php echo RUTA?>consult_recipe.php?txtID=<?php echo $patient_info["patient_id"]?>" class="cancel_button" value="Cancelar">Cancelar</a>
+            
+        </div>
+        
+    </form>
+</div>
+<?php require("templates/footer.php") ?>
