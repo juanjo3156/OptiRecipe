72050a575644df2e9274efd41af8b86d6e4965af
Juan José Ruiz Cruz <rcjuanjo3156@gmail.com>
2023-05-12 12:23:57 -0600

    agregando la restauración de los datos por default de la empresa

---
+++ scripts/restore_default.js
@@ -0,0 +1,12 @@
+
+function restoreDefaultValues() {
+    // Obtener los elementos de los campos del formulario
+    var enterpriseNameInput = document.getElementById("enterprise_name");
+    var addressInput = document.getElementById("address");
+    var phoneNumberInput = document.getElementById("phone");
+
+    // Restablecer los valores por defecto
+    enterpriseNameInput.value = 'OptiRecipe';
+    addressInput.value = 'No definido';
+    phoneNumberInput.value = 'No definido';
+}
